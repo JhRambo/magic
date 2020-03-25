e16934f77d366fa5a009ca55e7a72ba406a23b9c
zqw <237230999@qq.com>
2020-03-25 20:14:18 +0800

    修改

--- index.php
+++ index.php
@@ -1,13 +1,12 @@
 <?php
-// use Imooc\Object;
 
-include 'Object.php';
+require 'MyObject.php';
 
-$obj = new Object();
+$obj = new MyObject();
 
 // $obj->title = 'abc';    //默认调用__set函数
 // echo $obj->title;       //默认调用__get函数
 // $obj->test();           //默认调用__call函数
 // $obj::test();           //默认调用__callStatic函数
 // echo $obj;              //默认调用__toString函数
-echo $obj();
+echo $obj();            //默认调用__invoke函数
